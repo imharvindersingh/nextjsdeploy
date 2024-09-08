685b6014f273bdaae5ea105d57040000d612acf8
imharvindersingh <imharvindersingh@gmail.com>
2024-09-08 15:42:10 +0530

    fix spelling errors

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
        
-       <h2 className={styles.h2}> Comming Soon </h2>
+       <h2 className={styles.h2}> Coming Soon </h2>
 
         {/* <div className={styles.ctas}>
           <a
